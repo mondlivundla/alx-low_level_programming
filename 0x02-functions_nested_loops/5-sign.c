625ba1c625b0ccdb14f9d3127700b1230d963881
mondlivundla <mondlivundla7@gmail.com>
2022-09-13 21:47:15 +0200

    perfect file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
 /**
- * print_sign -> prints out sign depending on condition met
- * @n: character argument
- * Return: returns 1, 0, -1
+ * print_sign - print sign of number
+ * @n: number to check
+ * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -12,14 +12,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0);
 	{
-		_putchar ('-');
-		return (-1);
+		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		_putchar(0);
-		return (0);
+		_putchar('-');
+		return (-1);
 	}
 }
