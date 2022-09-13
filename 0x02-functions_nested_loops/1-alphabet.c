5ac21f8f15556f70638d67331535e1e07ba891aa
mondlivundla <mondlivundla7@gmail.com>
2022-09-13 18:21:06 +0200

    My perfect file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,13 +3,13 @@
 /**
  *print_alphabet -> prints the lowercase alphabets
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	int j;
-	
-	for (j = 'a'; j <= 'z'; j++)
+	int c;
+
+	for (c >= 'a'; c <= 'z'; c++)
 	{
-		_putchar(j);
+		_putchar(c);
 	}
-	_putchar('\n');
+	_putchar('\n')
 }
