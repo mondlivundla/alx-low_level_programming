2c53a6bddaabfe99f60ea14d5ed7f75a7734af9a
mondlivundla <mondlivundla7@gmail.com>
2022-09-21 22:19:58 +0200

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * Filename: main.h
+ * Author: Mondliwethu Vundla
+ * Filetype: headerfile
+ */
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -10,5 +15,8 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-
-#endif /*MAIN_H*/
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+#endif
