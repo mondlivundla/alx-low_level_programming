e6f375bbb5116a92fff5f9eb0c35a16fa07c8816
mondlivundla <mondlivundla7@gmail.com>
2022-09-26 21:35:35 +0200

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-        unsigned int i;
+        unsigned int a;
 
-        for (i = 0; n > 0 ; i++, n--)
+        for (a = 0; a < n; a++)
         {
-                dest[i] = src[i];
+                dest[a] = src[a];
         }
 
         return (dest);
