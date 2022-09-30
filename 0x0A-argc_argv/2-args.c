c76139fb959eb0116e65a5bbb4eae03e4a3004dd
mondlivundla <mondlivundla7@gmail.com>
2022-09-30 18:46:19 +0200

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,15 +10,17 @@
 
 int main(int argc, char *argv[])
 {
-	/*Declaring variables*/
-	int count = 0;
+/*Declaring variables*/
+int count = 0;
 
-	if (argc > 0)
-	{
-		/*WHILE - Print each arguments*/
-		while (count < argc)
-		{
-			printf("%s\n", argv[count]);
-		}
-	}
+if (argc > 0)
+{
+/*WHILE - Print each arguments*/
+while (count < argc)
+{
+printf("%s\n", argv[count]);
+count++;
+}
+}
+return (0);
 }
