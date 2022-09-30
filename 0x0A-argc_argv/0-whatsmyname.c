33a5cdbcbc519ce001b23e8ff5ac2ebf8325e42e
mondlivundla <mondlivundla7@gmail.com>
2022-09-30 18:39:50 +0200

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
-*main -> Prints name of program
-*@argc -> Count arguments
-*@argv -> Arguments used
-*Auth -> Mondliwethu Vundla
-*Return: Always 0(Success)
-*/
+ * main -> Prints name of program
+ * @argc: Count arguments
+ * @argv: Arguments used
+ * Auth -> Mondliwethu Vundla
+ * Return: Always 0(Success)
+ */
 
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+(void)argc;
+printf("%s\n", argv[0]);
 
-	return (0);
+return (0);
 }
