d2aff008ac13ce6da680ea8e1bb5fff1f90b15de
mondlivundla <mondlivundla7@gmail.com>
2022-09-30 12:31:38 +0200

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*main -> Prints name of program
+*@argc -> Count arguments
+*@argv -> Arguments used
+*Auth -> Mondliwethu Vundla
+*Return: Always 0(Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
