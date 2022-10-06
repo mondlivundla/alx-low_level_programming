6b8e4c74b13a1154ac550f72131756a504749a9a
mondlivundla <mondlivundla7@gmail.com>
2022-10-06 14:33:58 +0100

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@
  * Return: char pointer to newly allocated place in memory
  */
 
-chaqr *string_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j, k;
 	char *s;
