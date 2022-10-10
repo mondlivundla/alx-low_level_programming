b23ce57b9d6bdfb0c61b08831e1609650788f8f7
Mondliwethu Vundla <70095612+mondlivundla@users.noreply.github.com>
2022-10-10 21:12:31 +0200

    All mandatory tasks

--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include "0-object_like_macro.h"
 #include "1-pi.h"
-#include "1-function_like_macro.h"
+#include "3-function_like_macro.h"
 #include "4-sum.h"
 
 #endif /*MAIN_H*/
