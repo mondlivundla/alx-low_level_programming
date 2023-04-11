70c9b6dde7b28dec5bb39fd3f9a5fb22c27f8d53
mondlivundla <mondlivundla7@gmail.com>
2023-04-11 23:50:31 +0200

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,15 +9,19 @@
  *
  * Return: -1 when successful.
  */
-int linear_search(int *array, size_t size, int value) {
-    if (array == NULL) {
-        return -1;
-    }
-    for (size_t i = 0; i < size; i++) {
-        printf("Comparing %d with %d\n", array[i], value);
-        if (array[i] == value) {
-            return i;
-        }
-    }
-    return -1;
+int linear_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("Comparing %d with %d\n", array[i], value);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
