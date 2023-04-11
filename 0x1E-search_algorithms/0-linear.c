7c00dc76467d3ac640910ffbb1f219c35583db15
mondlivundla <mondlivundla7@gmail.com>
2023-04-11 23:35:53 +0200

    0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,20 @@
+/**
+ * linear_search - searches for a value in an array of integers
+ * @array: integer of arrays
+ * @value: integer value
+ * @size: size of array
+ *
+ * Return: -1 when successful.
+ */
+int linear_search(int *array, size_t size, int value) {
+    if (array == NULL) {
+        return -1;
+    }
+    for (size_t i = 0; i < size; i++) {
+        printf("Comparing %d with %d\n", array[i], value);
+        if (array[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+}
